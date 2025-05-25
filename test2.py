8ce0599a0482172c569d97f622177bf531ea0c68
Jhonattan-rocha <jhonattan246rocha@gmail.com>
2025-05-25 11:40:16 -0300

    Salvando

--- test2.py
+++
@@ -1,6 +0,0 @@
-import serial
-
-ser = serial.serial_for_url('loop://1', baudrate=9600, bytesize=8, parity="E", stopbits=2, rtscts=False, dsrdtr=False, timeout=5)
-ser.write(b'Teste teste teste teste')
-
-ser.close()
